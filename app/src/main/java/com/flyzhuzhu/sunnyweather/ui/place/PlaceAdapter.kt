51a4e23ed4c710851cab3692a9f75fcea9ebd50e
Tony <1483053880@qq.com>
2021-12-03 21:43:47 +0800

    新增切换城市和手动更换天气的功能

--- app/src/main/java/com/flyzhuzhu/sunnyweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/flyzhuzhu/sunnyweather/ui/place/PlaceAdapter.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.flyzhuzhu.sunnyweather.MainActivity
 import com.flyzhuzhu.sunnyweather.R
 import com.flyzhuzhu.sunnyweather.logic.model.Place
 import com.flyzhuzhu.sunnyweather.ui.weather.WeatherActivity
+import kotlinx.android.synthetic.main.activity_weather.*
 
 class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: List<Place>) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
 
@@ -24,14 +26,23 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: L
         holder.itemView.setOnClickListener {
             val position = holder.adapterPosition
             val place = placeList[position]
-            val intent = Intent(parent.context, WeatherActivity::class.java).apply {
-                putExtra("location_lng", place.location.lng)
-                putExtra("location_lat", place.location.lat)
-                putExtra("place_name", place.name)
+            val activity = fragment.activity
+            if (activity is WeatherActivity) {
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationLng = place.location.lng
+                activity.viewModel.locationLat = place.location.lat
+                activity.viewModel.placeName = place.name
+                activity.refreshWeather()
+            } else {
+                val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                    putExtra("location_lng", place.location.lng)
+                    putExtra("location_lat", place.location.lat)
+                    putExtra("place_name", place.name)
+                }
+                fragment.startActivity(intent)
+                activity?.finish()
             }
             fragment.viewModel.savePlace(place)
-            fragment.startActivity(intent)
-            fragment.activity?.finish()
         }
         return holder
     }
@@ -42,8 +53,6 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: L
         holder.placeAddress.text = place.address
     }
 
-    override fun getItemCount(): Int {
-        return placeList.size
-    }
+    override fun getItemCount() = placeList.size
 
 }
--- app/src/main/java/com/flyzhuzhu/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/flyzhuzhu/sunnyweather/ui/place/PlaceFragment.kt
@@ -13,6 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.flyzhuzhu.sunnyweather.MainActivity
 import com.flyzhuzhu.sunnyweather.R
 import com.flyzhuzhu.sunnyweather.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
@@ -36,7 +37,7 @@ class PlaceFragment : Fragment() {
     @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (viewModel.isPlaceSaved()) {
+        if (activity is MainActivity && viewModel.isPlaceSaved()) {
             val place = viewModel.getSavedPlace()
             val intent = Intent(context, WeatherActivity::class.java).apply {
                 putExtra("location_lng", place.location.lng)
